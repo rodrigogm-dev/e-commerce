773d55f277c251eeadae48fde1f1e04812db4823
rodrigo gayoso <rodrigogm.dev@gmail.com>
2025-03-25 17:51:04 -0500

    added load products

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './ProductList.css'
 
 function ProductList () {
   const [products, setProducts] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
 
   async function requestProducs () {
     const url = `https://fakestoreapi.com/products`
@@ -10,13 +11,37 @@ function ProductList () {
       const response = await fetch(url)
       const data = await response.json()
       setProducts(data)
-      console.log(data)
     } catch (error) {
-      console.error('Error al solicitar productos')
+      setErrorMessage(
+        'Lista de productos no disponible. Inténtelo de nuevo más tarde.'
+      )
+      console.error('Error al solicitar los productos')
     }
   }
 
-  return <div className='main-content'>{console.log(products)}</div>
+  useEffect(() => {
+    requestProducs()
+  }, [])
+
+  return (
+    <div className='main-content'>
+      {errorMessage ? (
+        <p className='error-message'>{errorMessage}</p>
+      ) : (
+        products.map(prod => (
+          <div key={prod.id} className='product-card'>
+            <p className='product-title'>{prod.title}</p>
+            <img
+              className='product-image'
+              src={prod.image}
+              alt={prod.title}
+            ></img>
+            <p className='product-price'>${prod.price}</p>
+          </div>
+        ))
+      )}
+    </div>
+  )
 }
 
 export default ProductList
